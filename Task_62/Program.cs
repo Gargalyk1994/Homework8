ee068cb0f72cfc0e78f04c4577525f5eb68a4f3f
Anton Gargalyk <gargalyk1994@gmail.com>
2023-03-11 19:37:36 +0900

    add Task_62

--- Task_62/Program.cs
+++ Task_62/Program.cs
@@ -4,3 +4,46 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите размер массива: ");
+int size = int.Parse(ReadLine());
+
+
+int[,] array1 = GetSpirale(size);
+Printresult(array1);
+WriteLine();
+
+
+int[,] GetSpirale(int n)
+{
+    int[,] result = new int[n, n];
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { result[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) result[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) result[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) result[i--, j] = value++;
+        ++i; ++j;//при этом отсчет начинается с единицы но не с нуля
+        n = n < 2 ? 0 : n - 2;
+    }
+    return result;
+}
+
+void Printresult(int[,] inresult)
+{
+    for (int i = 0; i < inresult.GetLength(0); i++)
+    {
+        for (int j = 0; j < inresult.GetLength(1); j++)
+        {
+            Write($"{inresult[i, j]} ");
+        }
+        WriteLine();
+    }
+}
